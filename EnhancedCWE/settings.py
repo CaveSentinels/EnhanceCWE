4e829cd4ee83844280ee839d1dcf7ff6988d2091
Walid Baruni <wdbaruni@gmail.com>
2015-06-05 15:20:19 -0400

    ADD: favicon

--- EnhancedCWE/settings.py
+++ EnhancedCWE/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'base',
     'admin_lte',
     'django_admin_bootstrapped',
     'autocomplete_light',
@@ -40,7 +41,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
     'cwe',
     'muo',
 )
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
