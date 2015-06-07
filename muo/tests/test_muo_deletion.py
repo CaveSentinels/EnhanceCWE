011243d0e03d116fd3add65fd105efa858bcdcd9
Anurag <anuragspn@gmail.com>
2015-06-07 15:14:08 -0400

    Fixing comments on masre.osoul.ly:8060/cru/MASRE-19

--- muo/tests/test_muo_deletion.py
+++ muo/tests/test_muo_deletion.py
@@ -1,62 +1,12 @@
 from django.test import TestCase
 from django.db import IntegrityError
-from cwe.models import Category, CWE
-from muo.models import MisuseCase, UseCase, OSR, MUOContainer
+from muo.models import MisuseCase, UseCase, OSR
 
 
-class TestDeletion(TestCase):
+class TestMUODeletion(TestCase):
     """
-    This class is the test suite to test the deletion behavior of the Category, CWE,
-    MisuseCase, UseCase, OSR and MUOContainer models.
+    This class is the test suite to test the deletion behavior of MisuseCase, UseCase and OSR
     """
-
-    def test_category_deletion_without_referring_cwes(self):
-        """
-        This is a positive test case
-        'delete()' should delete the category that has not been referred by any CWE.
-        """
-
-        category = Category.objects.create(name='category1')
-        category.delete()
-        self.assertEqual(Category.objects.count(), 0)
-
-
-    def test_category_deletion_with_referring_cwes(self):
-        """
-        This is a negative test case
-        'delete()' should raise the IntegrityError when trying to delete the category
-        that has been referred by one or more CWE.
-        """
-
-        category = Category.objects.create(name='category1')
-        cwe = CWE.objects.create(code=123, name='CWE1')
-        cwe.categories.add(category)
-        self.assertRaises(IntegrityError, category.delete)
-
-
-    def test_cwe_deletion_without_referring_misuse_cases(self):
-        """
-        This is a positive test case
-        'delete()' should delete the CWE that has not been referred by any MisuseCase.
-        """
-
-        cwe = CWE.objects.create(code=123, name='CWE1')
-        cwe.delete()
-        self.assertEqual(CWE.objects.count(), 0)
-
-
-    def test_cwe_deletion_with_referring_misuse_cases(self):
-        """
-        This is a negative test case
-        'delete()' should raise the IntegrityError when trying to delete the CWE that has
-        been referred by one or more MisuseCases.
-        """
-
-        cwe = CWE.objects.create(code=123, name='CWE1')
-        misuse_case = MisuseCase.objects.create(description='misuse_case')
-        misuse_case.cwes.add(cwe)
-        self.assertRaises(IntegrityError, cwe.delete)
-
 
     def test_misuse_case_deletion_without_referring_use_cases(self):
         """
