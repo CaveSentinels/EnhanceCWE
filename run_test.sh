0fc1e100e366ec3a1c862b793980fb8360d1206e
robin <robin.wyb@gmail.com>
2015-06-09 13:36:32 -0400

    [Update] Fix the problem that return values are not caught.

--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,9 @@
 echo "This script tests the project."
 
 # Run the "cwe" tests
-ret = $(python manage.py test cwe --settings=EnhancedCWE.test_settings)
+ret=$(python manage.py test cwe --settings=EnhancedCWE.test_settings)
 echo $ret
 
 # Run the "muo" tests
-ret = $(python manage.py test muo --settings=EnhancedCWE.test_settings)
+ret=$(python manage.py test muo --settings=EnhancedCWE.test_settings)
 echo $ret
