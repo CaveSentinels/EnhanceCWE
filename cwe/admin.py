bce1d113ce0f4204c5a6ef7a7f3d9a3d264d7913
Walid Baruni <wdbaruni@gmail.com>
2015-06-12 11:30:14 -0400

    ADD: stemming keyword names before saving them

--- cwe/admin.py
+++ cwe/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from base.admin import BaseAdmin, admin_site
 from models import *
@@ -9,8 +10,31 @@ class CategoryAdmin(BaseAdmin):
     search_fields = ['name']
 
 
+class KeywordAdminForm(forms.ModelForm):
+    """
+    Creating a custom form for keyword to validate the name is a single word only
+    """
+    def clean_name(self):
+        if len(self.cleaned_data["name"].split()) > 1:
+            raise forms.ValidationError("Keyword name should only have a single word")
+
+        """ Check the keyword stemmed format doesn't already exist in the database """
+        cwe_search = CWESearchLocator.get_instance()
+        stemmed_name = cwe_search.stem_text([self.cleaned_data["name"].lower()])
+        if stemmed_name:
+            # stem_text() always returns a list. Get the first element
+            stemmed_name = stemmed_name[0]
+            if Keyword.objects.filter(name__exact=stemmed_name).exists():
+                raise forms.ValidationError("Keyword stemmed name (%s) already exist!" % stemmed_name)
+            else:
+                self.cleaned_data["name"] = stemmed_name
+
+        return self.cleaned_data["name"]
+
+
 @admin.register(Keyword, site=admin_site)
 class KeywordAdmin(BaseAdmin):
+    form = KeywordAdminForm
     fields = ['name']
     search_fields = ['name']
 
