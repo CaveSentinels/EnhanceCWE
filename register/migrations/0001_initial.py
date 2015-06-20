cd9ddcdc2adb5e1715b7d4b63db16444a8a551a4
Sankalp Anand <sankalp.shopping@gmail.com>
2015-06-20 04:13:22 -0400

    EDIT: Handled registration migration file for the case of makemigration

--- register/migrations/0001_initial.py
+++ register/migrations/0001_initial.py
@@ -2,6 +2,18 @@
 from __future__ import unicode_literals
 from django.db import models, migrations
 
+def check_and_add_column(apps, schema_editor):
+    import sqlite3
+    conn = sqlite3.connect('db7.sqlite3')
+    cur = conn.cursor()
+    columns = [i[1] for i in cur.execute('PRAGMA table_info(auth_group)')]
+
+    if 'is_auto_assign' not in columns:
+        cur.execute('ALTER TABLE auth_group ADD COLUMN is_auto_assign BOOLEAN DEFAULT FALSE')
+
+
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -9,5 +21,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL("ALTER TABLE auth_group ADD COLUMN is_auto_assign BOOLEAN DEFAULT FALSE"),
+        migrations.RunPython(check_and_add_column,),
+
+        # If you need to run SQL directly from here
+        # migrations.RunSQL("ALTER TABLE auth_group ADD COLUMN is_auto_assign BOOLEAN DEFAULT FALSE"),
     ]
+
+
+
+
