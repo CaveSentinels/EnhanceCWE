cd3133691eb9bc5584148bbd6eaa09825860929e
Sankalp Anand <sankalp.shopping@gmail.com>
2015-07-03 01:15:29 -0400

    EDIT: First review comments implemented

--- invitation/admin.py
+++ invitation/admin.py
@@ -1,100 +1,10 @@
 from django.contrib import admin
-from django.contrib import messages
-from django.contrib.admin.templatetags.admin_urls import add_preserved_filters
-from django.contrib.admin.utils import (
-    quote, )
-from django.http import HttpResponseRedirect
-from django.template.response import SimpleTemplateResponse
-from django.utils.encoding import force_text
-from django.utils.html import escape, escapejs
-from django.utils.translation import ugettext as _
-from django.utils.crypto import get_random_string
-from django.core.urlresolvers import reverse
-from allauth.utils import build_absolute_uri
-from allauth.account import app_settings
-
 from models import *
 from base.admin import BaseAdmin
-
-
-IS_POPUP_VAR = '_popup'
-TO_FIELD_VAR = '_to_field'
-
 
 # Register your models here.
 @admin.register(EmailInvitation)
 class EmailInvitationAdmin(BaseAdmin):
-    fields = ['email', 'key']
-    readonly_fields = ['key']
-
-
-    def response_add(self, request, obj, post_url_continue=None):
-        """
-        Determines the HttpResponse for the add_view stage.
-        """
-        opts = obj._meta
-        pk_value = obj._get_pk_val()
-        preserved_filters = self.get_preserved_filters(request)
-        msg_dict = {'name': force_text(opts.verbose_name), 'obj': force_text(obj)}
-        # Here, we distinguish between different save types by checking for
-        # the presence of keys in request.POST.
-
-        if IS_POPUP_VAR in request.POST:
-            to_field = request.POST.get(TO_FIELD_VAR)
-            if to_field:
-                attr = str(to_field)
-            else:
-                attr = obj._meta.pk.attname
-            value = obj.serializable_value(attr)
-            return SimpleTemplateResponse('admin/popup_response.html', {
-                'pk_value': escape(pk_value),  # for possible backwards-compatibility
-                'value': escape(value),
-                'obj': escapejs(obj)
-            })
-
-        elif "_continue" in request.POST:
-            msg = _('The %(name)s "%(obj)s" was added successfully. You may edit it again below.') % msg_dict
-            self.message_user(request, msg, messages.SUCCESS)
-            if post_url_continue is None:
-                post_url_continue = reverse('admin:%s_%s_change' %
-                                            (opts.app_label, opts.model_name),
-                                            args=(quote(pk_value),),
-                                            current_app=self.admin_site.name)
-            post_url_continue = add_preserved_filters(
-                {'preserved_filters': preserved_filters, 'opts': opts},
-                post_url_continue
-            )
-            return HttpResponseRedirect(post_url_continue)
-
-        elif "_addanother" in request.POST:
-            msg = _('The %(name)s "%(obj)s" was added successfully. You may add another %(name)s below.') % msg_dict
-            self.message_user(request, msg, messages.SUCCESS)
-            redirect_url = request.path
-            redirect_url = add_preserved_filters({'preserved_filters': preserved_filters, 'opts': opts}, redirect_url)
-            return HttpResponseRedirect(redirect_url)
-
-        else:
-            email_add = request.POST['email']
-
-            random_key = obj.key
-            activate_url = '/accounts/signup?token='+ random_key + '&email='+email_add
-            activate_url = build_absolute_uri(request, activate_url, protocol=app_settings.DEFAULT_HTTP_PROTOCOL)
-
-
-            message_body = 'The EnhancedCWE Application admin has sent an invitation to join. Please click the following URL to sign the registration process \n' + activate_url
-
-            from django.core.mail import EmailMessage
-            email = EmailMessage('Enhanced CWE Invite', message_body, to=[email_add])
-            email.send()
-
-            msg = _('The email invitation was sent successfully to ' + email_add)
-            self.message_user(request, msg, messages.SUCCESS)
-            return self.response_post_save_add(request, obj)
-
-
-
-
-
-
-
-
+    fields = ['email', 'key', ('created_by', 'created_at')]
+    readonly_fields = ['key', 'created_by', 'created_at']
+    list_display = ['email', 'created_by', 'created_at']
--- invitation/models.py
+++ invitation/models.py
@@ -1,16 +1,71 @@
 from django.db import models
 from django.dispatch import receiver
 from django.utils.crypto import get_random_string
+from base.models import BaseModel
+from django.core.mail import send_mail
+from django.template.loader import get_template
+from django.template import Context
+from emailer import constants
+from EnhancedCWE import settings
+from django.core.urlresolvers import reverse
 
-class EmailInvitation(models.Model):
-    email = models.CharField(max_length=100)
-    key = models.CharField(verbose_name='key', max_length=64, unique=True)
+class EmailInvitation(BaseModel):
+    email = models.EmailField(max_length=100, db_index=True)
+    key = models.CharField(verbose_name='key', max_length=64, db_index=True, unique=True)
 
 
 from django.db.models.signals import post_save, pre_save
 
 
-@receiver(pre_save, sender=EmailInvitation, dispatch_uid='usecase_post_save_signal')
-def update_key(sender, instance, *args, **kwargs):
+@receiver(pre_save, sender=EmailInvitation, dispatch_uid='emailinvite_pre_save_signal')
+def pre_save_update_key(sender, instance, *args, **kwargs):
+    """
+    Since the key is generated randomly at runtime, this class method is called for attaching
+    the key to the current invitation instance.
+    """
     random_key = get_random_string(64).lower()
     instance.key = random_key
+
+@receiver(post_save, sender=EmailInvitation, dispatch_uid='emailinvite_post_save_signal')
+def post_save_send_email(sender, instance, created, using, **kwargs):
+    """
+    This method is used to invoke send_email() to send the invitation email to the new user after he is saved in the db
+    """
+
+    random_key = instance.key
+    email_add = instance.email
+    subject = 'Enhanced CWE Invite'
+    site_path = reverse("account_signup")
+    machine_add = current_site_url()
+    send_email(instance, subject, [email_add], random_key, site_path, machine_add)
+
+def send_email(instance, subject, email, key, site_path, machine_add):
+    """
+    This method is used to send email
+    :param: subject -the subject line of the email
+    :param: email address list - the list of email addresses
+    :param: key - the random generated authentication key
+    :param: site_path - the application path
+    :param: machine_add - the machine IP
+    """
+    if email:
+        send_mail(subject, get_template('invitation/invitation_invite_email.html').render(
+            Context({
+                'key': key,
+                'email': email[0],
+                'site_path': site_path,
+                'machine_add': machine_add
+                })
+            ), constants.SENDER_EMAIL, email, fail_silently=True)
+
+
+def current_site_url():
+    """Returns fully qualified URL (no trailing slash) for the current site."""
+    from django.contrib.sites.models import Site
+    current_site = Site.objects.get_current()
+    protocol = getattr(settings, 'MY_SITE_PROTOCOL', 'http')
+    port     = getattr(settings, 'MY_SITE_PORT', '')
+    url = '%s://%s' % (protocol, current_site.domain)
+    if port:
+        url += ':%s' % port
+    return url
