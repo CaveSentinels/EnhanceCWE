2b7b76e6cd3b78d9fd583770c584ebe6961a433a
Walid Baruni <wdbaruni@gmail.com>
2015-07-03 02:03:47 -0400

    Walid minor changes

--- invitation/admin.py
+++ invitation/admin.py
@@ -8,3 +8,6 @@ class EmailInvitationAdmin(BaseAdmin):
     fields = ['email', 'key', ('created_by', 'created_at')]
     readonly_fields = ['key', 'created_by', 'created_at']
     list_display = ['email', 'created_by', 'created_at']
+    search_fields = ['email', 'created_at']
+    list_filter = ['created_at']
+    date_hierarchy = 'created_at'
--- invitation/models.py
+++ invitation/models.py
@@ -5,16 +5,18 @@ from base.models import BaseModel
 from django.core.mail import send_mail
 from django.template.loader import get_template
 from django.template import Context
+from django.db.models.signals import post_save, pre_save
 from emailer import constants
 from EnhancedCWE import settings
 from django.core.urlresolvers import reverse
 
+
 class EmailInvitation(BaseModel):
     email = models.EmailField(max_length=100, db_index=True)
-    key = models.CharField(verbose_name='key', max_length=64, db_index=True, unique=True)
+    key = models.CharField(verbose_name='key', max_length=64, db_index=True)
 
-
-from django.db.models.signals import post_save, pre_save
+    def __unicode__(self):
+        return "to %s" % self.email
 
 
 @receiver(pre_save, sender=EmailInvitation, dispatch_uid='emailinvite_pre_save_signal')
@@ -26,45 +28,35 @@ def pre_save_update_key(sender, instance, *args, **kwargs):
     random_key = get_random_string(64).lower()
     instance.key = random_key
 
+
 @receiver(post_save, sender=EmailInvitation, dispatch_uid='emailinvite_post_save_signal')
 def post_save_send_email(sender, instance, created, using, **kwargs):
     """
     This method is used to invoke send_email() to send the invitation email to the new user after he is saved in the db
     """
-
-    random_key = instance.key
-    email_add = instance.email
-    subject = 'Enhanced CWE Invite'
+    key = instance.key
+    email = instance.email
+    subject = 'Enhanced CWE Invitation'
     site_path = reverse("account_signup")
-    machine_add = current_site_url()
-    send_email(instance, subject, [email_add], random_key, site_path, machine_add)
+    site_url = current_site_url()
 
-def send_email(instance, subject, email, key, site_path, machine_add):
-    """
-    This method is used to send email
-    :param: subject -the subject line of the email
-    :param: email address list - the list of email addresses
-    :param: key - the random generated authentication key
-    :param: site_path - the application path
-    :param: machine_add - the machine IP
-    """
-    if email:
-        send_mail(subject, get_template('invitation/invitation_invite_email.html').render(
-            Context({
-                'key': key,
-                'email': email[0],
-                'site_path': site_path,
-                'machine_add': machine_add
-                })
-            ), constants.SENDER_EMAIL, email, fail_silently=True)
+    send_mail(subject, get_template('invitation/invitation_invite_email.html').render(
+        Context({
+            'key': key,
+            'email': email,
+            'site_path': site_path,
+            'site_url': site_url
+        })
+    ), constants.SENDER_EMAIL, [email], fail_silently=True)
 
 
 def current_site_url():
     """Returns fully qualified URL (no trailing slash) for the current site."""
     from django.contrib.sites.models import Site
+
     current_site = Site.objects.get_current()
     protocol = getattr(settings, 'MY_SITE_PROTOCOL', 'http')
-    port     = getattr(settings, 'MY_SITE_PORT', '')
+    port = getattr(settings, 'MY_SITE_PORT', '')
     url = '%s://%s' % (protocol, current_site.domain)
     if port:
         url += ':%s' % port
